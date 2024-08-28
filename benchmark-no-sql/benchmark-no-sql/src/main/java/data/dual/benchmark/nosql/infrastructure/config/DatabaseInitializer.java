2ecd14b82b208228ba3832162e029fc51da22883
Samar Benamar <samar_benamar@ext.carrefour.com>
2024-08-28 11:10:45 +0100

    init database with java faker using @component

--- benchmark-no-sql/benchmark-no-sql/src/main/java/data/dual/benchmark/nosql/infrastructure/config/DatabaseInitializer.java
+++ benchmark-no-sql/benchmark-no-sql/src/main/java/data/dual/benchmark/nosql/infrastructure/config/DatabaseInitializer.java
@@ -3,42 +3,47 @@ package data.dual.benchmark.nosql.infrastructure.config;
 import com.github.javafaker.Faker;
 import data.dual.benchmark.nosql.domain.model.Product;
 import data.dual.benchmark.nosql.infrastructure.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
-public class DatabaseInitializer {
+@Slf4j
+@Component
+public class DatabaseInitializer implements CommandLineRunner {
 
     @Value("${database.max-size}")
     private int maxSize;
 
-    @Bean
-    CommandLineRunner initDatabase(ProductRepository productRepository) {
-        productRepository.deleteAll();
-        return args -> {
-            Faker faker = new Faker();
+    private final ProductRepository productRepository;
 
-            // Create products
-            for (int i = 0; i < maxSize; i++) {
-                String name = faker.commerce().productName();
-                String description = faker.lorem().sentence();
-                double price = Double.parseDouble(faker.commerce().price());
-                String category = faker.commerce().department();
-                int stock = faker.number().numberBetween(0, 100);
-
-                Product product = Product.builder()
-                        .name(name)
-                        .category(category)
-                        .price(price)
-                        .description(description)
-                        .stock(stock)
-                        .build();
-                productRepository.save(product);
-            }
-
-            System.out.println("Database initialized with random products");
-        };
+    public DatabaseInitializer(ProductRepository productRepository) {
+        this.productRepository = productRepository;
     }
+
+    @Override
+   public void run(String... args) {
+        productRepository.deleteAll();
+
+        Faker faker = new Faker();
+
+        for (int i = 0; i < maxSize; i++) {
+            String name = faker.commerce().productName();
+            String description = faker.lorem().sentence();
+            double price = Double.parseDouble(faker.commerce().price());
+            String category = faker.commerce().department();
+            int stock = faker.number().numberBetween(0, 100);
+
+            Product product = Product.builder()
+                    .name(name)
+                    .category(category)
+                    .price(price)
+                    .description(description)
+                    .stock(stock)
+                    .build();
+            productRepository.save(product);
+        }
+
+        log.info("Database initialized with random products");
+   }
 }
--- benchmark-sql/benchmark-sql/src/main/java/data_dual/benchmark_sql/infrastructure/config/DatabaseInitializer.java
+++ benchmark-sql/benchmark-sql/src/main/java/data_dual/benchmark_sql/infrastructure/config/DatabaseInitializer.java
@@ -3,42 +3,47 @@ package data_dual.benchmark_sql.infrastructure.config;
 import com.github.javafaker.Faker;
 import data_dual.benchmark_sql.domain.model.Product;
 import data_dual.benchmark_sql.infrastructure.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
-public class DatabaseInitializer {
+@Slf4j
+@Component
+public class DatabaseInitializer implements CommandLineRunner {
 
     @Value("${database.max-size}")
     private int maxSize;
 
-    @Bean
-    CommandLineRunner initDatabase(ProductRepository productRepository) {
+    private final ProductRepository productRepository;
+
+    public DatabaseInitializer(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public void run(String... args) {
         productRepository.deleteAll();
-        return args -> {
-            Faker faker = new Faker();
 
-            // Create products
-            for (int i = 0; i < maxSize; i++) {
-                String name = faker.commerce().productName();
-                String description = faker.lorem().sentence();
-                double price = Double.parseDouble(faker.commerce().price());
-                String category = faker.commerce().department();
-                int stock = faker.number().numberBetween(0, 100);
+        Faker faker = new Faker();
 
-                Product product = Product.builder()
-                        .name(name)
-                        .category(category)
-                        .price(price)
-                        .description(description)
-                        .stock(stock)
-                        .build();
-                productRepository.save(product);
-            }
+        for (int i = 0; i < maxSize; i++) {
+            String name = faker.commerce().productName();
+            String description = faker.lorem().sentence();
+            double price = Double.parseDouble(faker.commerce().price());
+            String category = faker.commerce().department();
+            int stock = faker.number().numberBetween(0, 100);
 
-            System.out.println("Database initialized with random products");
-        };
+            Product product = Product.builder()
+                    .name(name)
+                    .category(category)
+                    .price(price)
+                    .description(description)
+                    .stock(stock)
+                    .build();
+            productRepository.save(product);
+        }
+
+        log.info("Database initialized with random products");
     }
 }
