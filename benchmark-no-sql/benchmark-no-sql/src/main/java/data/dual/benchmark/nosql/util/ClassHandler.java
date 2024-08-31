2b08863d111a4604ba6b9c878cacaa5f823a282f
ng-bilel <bilel.feki.92@gmail.com>
2024-08-31 12:14:58 +0100

    feat(util): create class util to handle for java reflection

---
+++ benchmark-no-sql/benchmark-no-sql/src/main/java/data/dual/benchmark/nosql/util/ClassHandler.java
@@ -0,0 +1,44 @@
+package data.dual.benchmark.nosql.util;
+
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+@Service
+@NoArgsConstructor
+public class ClassHandler<T> {
+    public Class<?> getReturnTypeClassOfMethod(Method method) {
+        return method.getReturnType();
+    }
+
+    /**
+     * usage : getAllCLassMethod(Product.class)
+     * @param classToWorkOn
+     * @return
+     */
+    public List<Method> getAllCLassMethod(Class<?> classToWorkOn) {
+        return List.of(classToWorkOn.getDeclaredMethods());
+    }
+
+    public List<String> extractMethodsName(List<Method> methods) {
+        return methods.stream().map(Method::getName).toList();
+    }
+
+    public List<String> extractGetterMethodNames(List<Method> methods) {
+        return methods.stream().map(Method::getName).filter(methodName -> methodName.startsWith("get")).toList();
+    }
+
+    public List<String> extractSetterMethodNames(List<Method> methods) {
+        return methods.stream().map(Method::getName).filter(methodName -> methodName.startsWith("set")).toList();
+    }
+
+    Class<?> createAnInstanceWithCLassName(String className) {
+        try {
+            return Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            return null;
+        }
+    }
+}
